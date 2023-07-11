1cece81b8f5bcd26638388796d9bbe8faf834c5a
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-11 19:01:06 -0300

    Update app.js

--- Api-rest/src/app.js
+++ Api-rest/src/app.js
@@ -2,12 +2,25 @@ import express from 'express'
 
 const app = express()
 
+//Mock
+const selecoes = [
+    {id:1, selecao:'Brasil', grupo:'G'},
+    {id:2, selecao:'Suica', grupo:'G'},
+    {id:3, selecao:'Servia', grupo:'G'},
+    {id:4, selecao:'Camaroes', grupo:'G'},
+]
+
 const port = 3000
 
 app.get('/', (req, res) => {
 
     res.send('Hello World!')
 
+})
+
+app.get('/selecoes', (req, res) => {
+
+    res.send(selecoes)
 })
 
 
