52d6bbf9b80870ff1e2a3dfd6b1728541028179b
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-18 17:08:19 -0300

    '

--- Api-rest/server.js
+++ Api-rest/server.js
@@ -1,19 +1,10 @@
 import app from "./src/app.js"
 
-import conexao from "./infra/conexao.js"
+
 const PORT = 3000
 
-conexao.connect( (erro) => {
-   if(erro){
-    console.log(erro)
-   }else{
-    console.log('Deu Bom')
-
-    app.listen(PORT, () =>{
-        console.log(`${PORT}`)
-    })
-   }
-
+app.listen(PORT, () =>{
+    console.log(`${PORT}`)
 })
 
 
--- Api-rest/src/app.js
+++ Api-rest/src/app.js
@@ -5,14 +5,6 @@ const app = express()
 
 app.use(express.json())
 
-//Mock
-const selecoes = [
-    {id:1, selecao:'Brasil', grupo:'G'},
-    {id:2, selecao:'Suica', grupo:'G'},
-    {id:3, selecao:'Servia', grupo:'G'},
-    {id:4, selecao:'Camaroes', grupo:'G'},
-]
-
 
 function buscarSelecaoPorId(id){
     return selecoes.filter(selecao => selecao.id == id)
