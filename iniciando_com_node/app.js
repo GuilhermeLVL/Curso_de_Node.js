3887745a46bdf77290e28333c79348a4490ecdc8
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-19 18:41:22 -0300

    Update app.js

--- iniciando_com_node/app.js
+++ iniciando_com_node/app.js
@@ -3,12 +3,7 @@ const express = require("express");
 
 const app = express()
 
-app.get("/primeira-rota",(request, response) => {
 
-    return response.json({
-        message:"Acessou a primeira rota"
-    })
-})
 
 //Definindo a porta padrao como 4002
 app.listen(4002,() => console.log('servidor esta rodando na porta 4002'))
