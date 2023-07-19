1b374e6eda11b50f6898dbb3beff35b64913814c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-19 18:36:09 -0300

    Primeira rota com Express

---
+++ iniciando_com_node/app.js
@@ -0,0 +1,12 @@
+//Inicializando o servidor com o Express
+const express = require("express");
+
+const app = express()
+
+app.get("/primeira-rota",(request, response) => {
+
+    return response.send("Acessou a primeira rota");
+})
+
+//Definindo a porta padrao como 4002
+app.listen(4002,() => console.log('servidor esta rodando na porta 4002'))
