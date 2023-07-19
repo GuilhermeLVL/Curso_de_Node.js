2691cac3b8add35fcb18239fa799adf4ecf68244
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-19 18:44:38 -0300

    criando post

--- iniciando_com_node/app.js
+++ iniciando_com_node/app.js
@@ -3,7 +3,20 @@ const express = require("express");
 
 const app = express()
 
+const products = [];
 
+/*
+post = inseriri um dado
+get = busca 1 ou mais dados
+put = alterar um dado
+Delete = remover um dado
+*/
+
+// url 
+app.post("/products",(request, response) => {
+    //Nome e Preco
+    
+})
 
 //Definindo a porta padrao como 4002
 app.listen(4002,() => console.log('servidor esta rodando na porta 4002'))
