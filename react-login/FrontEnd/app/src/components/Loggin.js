e00e63288daaf50cd3dc0bcea2204a9e7a0892d5
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 18:10:22 -0300

    Update Loggin.js

--- react-login/FrontEnd/app/src/components/Loggin.js
+++ react-login/FrontEnd/app/src/components/Loggin.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 function Login(){
+
+const [email, setEmail] = useState ('')
+const [password, setPassword] = useState ('')
 
 const handleLogin = async (e) => {
     e.preventDefault();
 
-    console.log('Loginm clicked');
+    console.log(email, password);
 }
 
 
@@ -13,9 +18,16 @@ const handleLogin = async (e) => {
     <div className='login-form-wrap'>
             <h2>Login</h2>
             <form className='login-form'>
-            <input type='email' name='email' placeholder='Email' required></input>
-            <input type='password' name='password' placeholder='Password' required></input>
-            <button type='submit' className='btn-login' onClick={(e) => handleLogin(e)}>Login</button>
+            <input type='email' name='email' placeholder='Email' required onChange={(e) => setEmail(e.target.value)}></input>
+            <input type='password' name='password' placeholder='Password' required
+            onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <button 
+            type='submit'
+            className='btn-login'
+            onClick={(e) => handleLogin(e)}
+            
+              >Login</button>
             
             </form>
           </div>
