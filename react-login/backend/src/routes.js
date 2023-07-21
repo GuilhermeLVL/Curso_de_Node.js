028341a78b0660ebe6cabb6a5fbeba542e24282c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 17:22:45 -0300

    Update routes.js

--- react-login/backend/src/routes.js
+++ react-login/backend/src/routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
 const routes = express.Router();
-
+const users = [{
+    id:1,
+    name:"Guilherme",
+    email:"guilherme@gmail.com ",
+    password:"123123"
+}]
 routes.post('/login',(req, res) => {
 
     const {email, password} = req.body;
-    res.send(email)
+
+    const user = users.find(user => user.email === email && user.password === password) ;
+
+    if(user){
+        res.status(200).json(user);
+    }
+
+    res.send(401).json({message: "Invalid"  });
 });
 
 module.exports = routes;
