59ed7ad395dfe7e171ba4cb9cad7d08ffa34ac5c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 18:00:26 -0300

    Update App.js

--- react-login/FrontEnd/app/src/App.js
+++ react-login/FrontEnd/app/src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className='login-form-wrap'>
+function Login(){
+
+  return(
+  <div className='login-form-wrap'>
           <h2>Login</h2>
           <form className='login-form'>
           <input type='email' name='email' placeholder='Email' required></input>
           <input type='password' name='password' placeholder='Password' required></input>
+          <button type='submit' className='btn-login'>Login</button>
           
           </form>
         </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Login></Login>
       </header>
     </div>
   );
