aa12e368fa7eb868b95eca23de62508b2f088b7d
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 17:56:46 -0300

    a

--- react-login/FrontEnd/app/src/App.js
+++ react-login/FrontEnd/app/src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className='login-form-wrap'>
+          <h2>Login</h2>
+          <form className='login-form'>
+          <input type='email' name='email' placeholder='Email' required></input>
+          <input type='password' name='password' placeholder='Password' required></input>
+          
+          </form>
+        </div>
       </header>
     </div>
   );
