7d814bf4cbeaf5f4ae78f739de2bdb59e0dea302
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 17:27:53 -0300

    Update routes.js

--- react-login/backend/src/routes.js
+++ react-login/backend/src/routes.js
@@ -1,23 +1,28 @@
 const express = require('express');
 
 const routes = express.Router();
+
+//Dados locais
 const users = [{
     id:1,
     name:"Guilherme",
-    email:"guilherme@gmail.com ",
-    password:"123123"
+    email:"guilherme@gmail.com",
+    password:"dasdasdas"
 }]
+
 routes.post('/login',(req, res) => {
 
+    //Dados da api(node)
     const {email, password} = req.body;
 
+    //Comparando os dados da api com os dados locaias
     const user = users.find(user => user.email === email && user.password === password) ;
 
     if(user){
-        res.status(200).json(user);
+       return res.status(200).json(user);
     }
 
-    res.send(401).json({message: "Invalid"  });
+    return res.send(401).json({message: "Invalid"  });
 });
 
 module.exports = routes;
