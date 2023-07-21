eba5081ef6468532e1c7feba22f8c517ef3a71b8
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-21 18:04:47 -0300

    Update Loggin.js

--- react-login/FrontEnd/app/src/components/Loggin.js
+++ react-login/FrontEnd/app/src/components/Loggin.js
@@ -1,12 +1,21 @@
 function Login(){
 
+const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log('Loginm clicked');
+}
+
+
+
+
     return(
     <div className='login-form-wrap'>
             <h2>Login</h2>
             <form className='login-form'>
             <input type='email' name='email' placeholder='Email' required></input>
             <input type='password' name='password' placeholder='Password' required></input>
-            <button type='submit' className='btn-login'>Login</button>
+            <button type='submit' className='btn-login' onClick={(e) => handleLogin(e)}>Login</button>
             
             </form>
           </div>
