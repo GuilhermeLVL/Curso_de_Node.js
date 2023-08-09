347c468191b792ef4dd639fff62b26f089a114ae
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-09 16:59:28 -0300

    .

--- Api-rest/src/app/controllers/SelecaoControler.js
+++ Api-rest/src/app/controllers/SelecaoControler.js
@@ -1,18 +1,13 @@
 import conexao from "../database/conexao.js"
+import selecaoRepository from "../repositories/selecaoRepository.js"
 
 class SelecaoControler {
     
 
     index(req, res) {
         // res.status(200).send(selecoes)
-        const sql = "SELECT * FROM selecoes;"
-        conexao.query(sql, (erro, resultado) => {
-            if(erro) {
-                res.status(404).json({ 'erro': erro})
-            } else {
-                res.status(200).json(resultado)
-            }
-        })
+       const row = selecaoRepository.findAll()
+       res.json(row)
     }
 
 
---
+++ Api-rest/src/app/repositories/selecaoRepository.js
@@ -0,0 +1,25 @@
+import conexao from "../database/conexao"
+
+class selecaoRepository{
+//crud
+create(){}
+
+findAll(){
+    const sql = "SELECT * FROM selecoes;"
+   return new Promise((resolve, reject) =>{
+    conexao.query(sql, (erro, resultado) => {
+        if (erro) return reject("Nao foi possivel localizar")
+
+        const row = JSON.parse(JSON.stringify(resultado))
+        return resolve(row)
+    })
+   })
+}
+
+findById(){}
+update(){}
+delete(){}
+
+}
+
+export default new selecaoRepository()
